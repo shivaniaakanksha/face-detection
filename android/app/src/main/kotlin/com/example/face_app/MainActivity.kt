5aa19ef432a97946de2aa6cc14d16f7ac3662170
Aakanksha Shivani <shivaniaakanksha@gmail.com>
2020-10-04 21:19:02 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/face_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.face_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,327 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:ui' as ui;
+import 'package:face_app/utils.dart';
+import 'package:image/image.dart' as imglib;
+import 'package:tflite_flutter/tflite_flutter.dart' as tfl;
+
+import 'package:camera/camera.dart';
+import 'package:face_app/detector_painters.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
+import 'utils.dart';
+import 'package:quiver/collection.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: FacePage(),
+    );
+  }
+}
+
+CameraController _camera;
+dynamic _scanResults;
+
+class FacePage extends StatefulWidget {
+  @override
+  _FacePageState createState() => _FacePageState();
+}
+
+class _FacePageState extends State<FacePage> {
+  CameraController _camera;
+  int facesCount;
+  File _imageFile;
+  List<Face> _faces;
+  bool isLoading = false;
+  ui.Image _image;
+  File jsonFile;
+  dynamic _scanResults;
+  var interpreter;
+  bool _isDetecting = false;
+  CameraLensDirection _direction = CameraLensDirection.front;
+  dynamic data = {};
+  double threshold = 1.0;
+  Directory tempDir;
+  List e1;
+  bool _faceFound = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+    _initializeCamera();
+  }
+
+  Future loadModel() async {
+    try {
+      final gpuDelegateV2 = tfl.GpuDelegateV2(
+          options: tfl.GpuDelegateOptionsV2(
+        false,
+        tfl.TfLiteGpuInferenceUsage.fastSingleAnswer,
+        tfl.TfLiteGpuInferencePriority.minLatency,
+        tfl.TfLiteGpuInferencePriority.auto,
+        tfl.TfLiteGpuInferencePriority.auto,
+      ));
+
+      var interpreterOptions = tfl.InterpreterOptions()
+        ..addDelegate(gpuDelegateV2);
+      interpreter = await tfl.Interpreter.fromAsset('mobilefacenet.tflite',
+          options: interpreterOptions);
+    } on Exception {
+      print('Failed to load model.');
+    }
+  }
+
+  void _initializeCamera() async {
+    await loadModel();
+    CameraDescription description = await getCamera(_direction);
+
+    ImageRotation rotation = rotationIntToImageRotation(
+      description.sensorOrientation,
+    );
+
+    _camera =
+        CameraController(description, ResolutionPreset.low, enableAudio: false);
+    await _camera.initialize();
+    await Future.delayed(Duration(milliseconds: 500));
+    tempDir = await getApplicationDocumentsDirectory();
+    String _embPath = tempDir.path + '/emb.json';
+    jsonFile = new File(_embPath);
+    if (jsonFile.existsSync()) data = json.decode(jsonFile.readAsStringSync());
+
+    _camera.startImageStream((CameraImage image) {
+      if (_camera != null) {
+        if (_isDetecting) return;
+        _isDetecting = true;
+        String res;
+        dynamic finalResult = Multimap<String, Face>();
+        detect(image, _getDetectionMethod(), rotation).then(
+          (dynamic result) async {
+            if (result.length == 0)
+              _faceFound = false;
+            else
+              _faceFound = true; //TODO: face found
+            Face _face;
+            imglib.Image convertedImage =
+                _convertCameraImage(image, _direction);
+            for (_face in result) {
+              double x, y, w, h;
+              x = (_face.boundingBox.left - 10);
+              y = (_face.boundingBox.top - 10);
+              w = (_face.boundingBox.width + 10);
+              h = (_face.boundingBox.height + 10);
+              imglib.Image croppedImage = imglib.copyCrop(
+                  convertedImage, x.round(), y.round(), w.round(), h.round());
+              croppedImage = imglib.copyResizeCropSquare(croppedImage, 112);
+              // int startTime = new DateTime.now().millisecondsSinceEpoch;
+              // res = _recog(croppedImage);
+              // int endTime = new DateTime.now().millisecondsSinceEpoch;
+              // print("Inference took ${endTime - startTime}ms");
+              finalResult.add(res, _face);
+            }
+            setState(() {
+              _scanResults = finalResult;
+            });
+
+            _isDetecting = false;
+          },
+        ).catchError(
+          (_) {
+            _isDetecting = false;
+          },
+        );
+      }
+    });
+  }
+
+  HandleDetection _getDetectionMethod() {
+    final faceDetector = FirebaseVision.instance.faceDetector(
+      FaceDetectorOptions(
+        mode: FaceDetectorMode.accurate,
+      ),
+    );
+    return faceDetector.processImage;
+  }
+
+  imglib.Image _convertCameraImage(
+      CameraImage image, CameraLensDirection _dir) {
+    int width = image.width;
+    int height = image.height;
+    // imglib -> Image package from https://pub.dartlang.org/packages/image
+    var img = imglib.Image(width, height); // Create Image buffer
+    const int hexFF = 0xFF000000;
+    final int uvyButtonStride = image.planes[1].bytesPerRow;
+    final int uvPixelStride = image.planes[1].bytesPerPixel;
+    for (int x = 0; x < width; x++) {
+      for (int y = 0; y < height; y++) {
+        final int uvIndex =
+            uvPixelStride * (x / 2).floor() + uvyButtonStride * (y / 2).floor();
+        final int index = y * width + x;
+        final yp = image.planes[0].bytes[index];
+        final up = image.planes[1].bytes[uvIndex];
+        final vp = image.planes[2].bytes[uvIndex];
+        // Calculate pixel color
+        int r = (yp + vp * 1436 / 1024 - 179).round().clamp(0, 255);
+        int g = (yp - up * 46549 / 131072 + 44 - vp * 93604 / 131072 + 91)
+            .round()
+            .clamp(0, 255);
+        int b = (yp + up * 1814 / 1024 - 227).round().clamp(0, 255);
+        // color: 0x FF  FF  FF  FF
+        //           A   B   G   R
+        img.data[index] = hexFF | (b << 16) | (g << 8) | r;
+      }
+    }
+    var img1 = (_dir == CameraLensDirection.front)
+        ? imglib.copyRotate(img, -90)
+        : imglib.copyRotate(img, 90);
+    return img1;
+  }
+
+  _getImageAndDetectFaces() async {
+    final imageFile = await ImagePicker.pickImage(source: ImageSource.camera);
+    setState(() {
+      isLoading = true;
+    });
+    final image = FirebaseVisionImage.fromFile(imageFile);
+    final faceDetector = FirebaseVision.instance.faceDetector();
+    List<Face> faces = await faceDetector.processImage(image);
+    if (mounted) {
+      setState(() {
+        _imageFile = imageFile;
+        _faces = faces;
+        _loadImage(imageFile);
+        facesCount = faces.length;
+      });
+    }
+  }
+
+  _loadImage(File file) async {
+    final data = await file.readAsBytes();
+    await decodeImageFromList(data).then(
+      (value) => setState(() {
+        _image = value;
+        isLoading = false;
+      }),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _buildImage(),
+      // isLoading
+      //     ? Center(child: CircularProgressIndicator())
+      //     : (_imageFile == null)
+      //         ? Center(child: Text('No image selected'))
+      //         : Center(
+      //             child: Column(
+      //               mainAxisAlignment: MainAxisAlignment.spaceAround,
+      //               children: [
+      //                 FittedBox(
+      //                   child: SizedBox(
+      //                     width: _image.width.toDouble(),
+      //                     height: _image.height.toDouble(),
+      //                     child: CustomPaint(
+      //                       painter: FacePainter(_image, _faces),
+      //                     ),
+      //                   ),
+      //                 ),
+      //                 facesCount == 0
+      //                     ? Text("No face Detected")
+      //                     : Text(
+      //                         "Face Detected",
+      //                         style: TextStyle(fontSize: 20),
+      //                       )
+      //               ],
+      //             ),
+      //           ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _getImageAndDetectFaces,
+        tooltip: 'Pick Image',
+        child: Icon(Icons.add_a_photo),
+      ),
+    );
+  }
+}
+
+Widget _buildImage() {
+  if (_camera == null || !_camera.value.isInitialized) {
+    return Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+
+  return Container(
+    constraints: const BoxConstraints.expand(),
+    child: _camera == null || !_camera.value.isInitialized
+        ? CircularProgressIndicator()
+        : Stack(
+            fit: StackFit.expand,
+            children: <Widget>[
+              CameraPreview(_camera),
+              _buildResults(),
+            ],
+          ),
+  );
+}
+
+Widget _buildResults() {
+  const Text noResultsText = const Text('');
+  if (_scanResults == null || _camera == null || !_camera.value.isInitialized) {
+    return noResultsText;
+  }
+  CustomPainter painter;
+
+  final Size imageSize = Size(
+    _camera.value.previewSize.height,
+    _camera.value.previewSize.width,
+  );
+  painter = FaceDetectorPainter(imageSize, _scanResults);
+  return CustomPaint(
+    painter: painter,
+  );
+}
+
+class FacePainter extends CustomPainter {
+  final ui.Image image;
+  final List<Face> faces;
+  final List<Rect> rects = [];
+  FacePainter(this.image, this.faces) {
+    for (var i = 0; i < faces.length; i++) {
+      rects.add(faces[i].boundingBox);
+    }
+  }
+
+  @override
+  void paint(ui.Canvas canvas, ui.Size size) {
+    final Paint paint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 15.0
+      ..color = Colors.yellow;
+
+    canvas.drawImage(image, Offset.zero, Paint());
+    for (var i = 0; i < faces.length; i++) {
+      canvas.drawRect(rects[i], paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(FacePainter oldDelegate) {
+    return image != oldDelegate.image || faces != oldDelegate.faces;
+  }
+}
